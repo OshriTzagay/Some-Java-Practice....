848c1d2e9ae6a8ae0e94fcbe97e61edc3117d70f
OshriTzagay <oshritzagay@gmail.com>
2021-08-08 23:31:57 +0300

    firstCommit

---
+++ main.js
@@ -0,0 +1,188 @@
+// function myName (){
+// for (var i=0;i<5;i++){
+    
+//     var myName="oshri";
+//     console.log(myName);
+// }
+// }
+// myName();
+////////////////////////////Mission2///////////////////
+// function myName(abc){
+// let myName = "Oshri"
+// for (var i = 0; i<5 ; i++){
+// console.log(myName);
+// }
+
+// }
+// myName();
+////////////////////////Mission 4///////////////////
+
+// function myNums (a,b){
+
+// console.log(a+b)
+// }
+// myNums(5 , 6);
+
+// /////////////////Mission 3////////////////////
+
+// function theNumbers (){
+//     var num1=10
+//     var num2=20
+// console.log(num1*num2);
+
+// }
+// theNumbers()
+
+////////////////MISSSION 5 /////////////
+
+// function theBiggerNum (a,b){
+
+// if (a > b){
+//     console.log(a);
+// }
+// else{
+//     console.log(b);
+// }
+// }
+// theBiggerNum(20 ,21);
+
+///////Mission 6////////////////////////////////
+
+// function splitNum (Num){
+// splitNum = 777;
+
+
+// }
+
+////////////Misssion 7///////////////
+
+// var arrayNumber = [22, 58, 47, 56, 95, 100];
+
+// function sumOfAllNum(arrayNumber) {
+//     var sum = 0;
+//     for (var i = 0; i < arrayNumber.length; i++) {
+//         sum = arrayNumber[i] + sum;
+//     }
+//     return(sum)
+// }
+
+// var x = sumOfAllNum(arrayNumber);
+// console.log(x);
+
+///////////////////////////Mission 7.1//////////////////////
+// var arrayNumber = [22, 58, 47, 56, 95, 100];
+
+// function sumOfAllNum(arrayNumber) {
+//     var sum = 1;
+//     for(i=0;i<arrayNumber.length;i++) {
+// sum = arrayNumber[i] * sum ;
+
+//     }
+//     return(sum)
+// }
+// var theKefel=sumOfAllNum(arrayNumber);
+// console.log(theKefel);
+
+
+/////////////////////////Mission 7.3/////////////////////////////
+
+
+
+// function minOfNumbers() {
+//     var arrayNumbers = [222, 584, 457, 56, 95, 10];
+//     var minNum = arrayNumbers[0];
+//     for(var i = 1;i<arrayNumbers.length;i++){
+// if (minNum > arrayNumbers[i]){
+// minNum = arrayNumbers[i];
+// }
+
+//     }
+// return minNum;
+// }    
+// minNum=minOfNumbers();
+// console.log(minNum);
+
+/////////////////////Mission 7.4////////////////
+
+// function MaxOfNumbers(){
+//     var arrayNumbers = [22, 55 , 32, 41,555,2];
+//     var maxNum = arrayNumbers[1];
+//     for(var i = 1 ; i<arrayNumbers.length;i++){
+// if (maxNum < arrayNumbers[i]){
+//     maxNum = arrayNumbers[i];
+
+// }
+//     }
+//     return maxNum;
+// }
+// maxNum = MaxOfNumbers();
+// console.log(maxNum);
+
+
+///////////////////POP()Mission//////////////////
+
+// function DeletedIndex () {
+// var namesArray = ["oshri","shimon","avi","eliyau"];
+// namesArray.pop();
+// namesArray.pop();
+// console.log(namesArray);
+
+
+// }
+// var popShit=DeletedIndex;
+// popShit();
+
+/////////////Length Of a String Mission (8)///////////////////
+
+// function myFunction (){
+// var myFunction ="OshriTheKing";
+// console.log(myFunction.length);
+
+// }
+// myFunction();
+// ////////////////////Mission (9)////////////////////////COME BACK HERE U MF
+
+// function tzugFunction (){
+//     var tzugArray = [23 , 24 , 25 ,26 , 27, 30];
+//     var userInput = prompt ("GET YOUR NUMBERS HERE NIGGA");
+// for ( var i = 0 ; i<tzugArray; i++){
+//     if (userInput == tzugArray[i]){
+// tzugArray.pop();
+// console.log("Already Got the Number.")
+
+//     }
+//     return tzugArray
+// }
+
+// }
+//     tzugFunction();
+
+// function myFunction (){
+// var theArray = [2,4,7,9,10];
+// for(var i = 0 ; i<theArray.length;i++){
+
+// theArray[i]= theArray[i]*3;
+
+// }
+// return theArray 
+// }
+// var x = myFunction;
+// console.log(x);    
+
+////////////////////////////CHANGEEEEEEEEEEEEEEEEEEEEEEEEEE//////////////////////////////
+
+
+
+
+// var arrayNumber = [22, 58, 47, 56, 95, 100];
+// var arrayNumber2= [10,20,30,40,50,60,70];
+
+// function functionArray (arrayNumber){
+// for (var i=0; i< arrayNumber.length ; i++){
+// arrayNumber[i]=arrayNumber[i]*3;
+// }
+// return (arrayNumber)
+// }
+
+// var x = functionArray(arrayNumber);
+// console.log(x);
